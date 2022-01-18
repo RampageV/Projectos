7e900756c25e537cc64409e60e06779bd81f809c
Erick Pessoa <erick.pessoa.2014@hotmail.com>
2022-01-18 18:26:46 -0300

    Atualização

--- Calculadora/src/Tela/Tela.java
+++ Calculadora/src/Tela/Tela.java
@@ -1,16 +1,46 @@
 package Tela;
 
+import java.awt.BorderLayout;
+import java.awt.Container;
+import java.awt.FlowLayout;
+
+import javax.swing.JButton;
 import javax.swing.JFrame;
 
 public class Tela extends JFrame {
 
+	private static final long serialVersionUID = 1L;
+
 	public Tela() {
 
-		JFrame janela = new JFrame("Nova janela");// Criando o objeto.
-		janela.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // Quando você fecha a janela, é para encerra a aplicação.
-		janela.setSize(400, 600); // Tamanho da janela
-		janela.setVisible(true); // Método para ficar visivel, caso for false a janela não é mostrada.
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // Se não tiver esse método, mesmo que você aperte x, ele vai
+														// está exercutando // está em exercução
+		setSize(400, 600); // Tamanho da janela
+		setLocationRelativeTo(null); // A calculadora abre no meio da tela.
+		setVisible(true); // Método para ficar visivel, caso for false a janela não é mostrada.
 
+		Container container = getContentPane();
+		container.setLayout(new FlowLayout());
+
+		JButton botao1 = new JButton("1");
+		container.add(botao1, BorderLayout.CENTER);
+
+		JButton botao2 = new JButton("2");
+		container.add(botao2, BorderLayout.WEST);
+
+		JButton botao3 = new JButton("3");
+		container.add(botao3, BorderLayout.EAST);
+
+		JButton botao4 = new JButton("4");
+		container.add(botao4);
+
+		JButton botao5 = new JButton("5");
+		container.add(botao5);
+
+		JButton botao6 = new JButton("6");
+		container.add(botao6);
+
+		setVisible(true);
 	}
 
 }
