470fee319af9b20d1dccb5d8344063dd629da98c
Erick Pessoa <erick.pessoa.2014@hotmail.com>
2022-01-28 22:59:56 -0300

    Atualizando metodo da calculadora

--- Calculadora/src/Main/Main.java
+++ Calculadora/src/Main/Main.java
@@ -5,7 +5,7 @@ import Tela.Tela;
 public class Main {
 
 	public static void main(String[] args) {
-	
+
 		new Tela();
 
 	}
--- Calculadora/src/Tela/Tela.java
+++ Calculadora/src/Tela/Tela.java
@@ -1,46 +1,54 @@
 package Tela;
 
 import java.awt.BorderLayout;
+import java.awt.Button;
 import java.awt.Container;
 import java.awt.FlowLayout;
+import java.awt.GridLayout;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
 
 public class Tela extends JFrame {
 
-	private static final long serialVersionUID = 1L;
-
+	/*Criação dos botões*/
+	Button botao0 = new Button("0");
+	Button botao1 = new Button("1");
+	Button botao2 = new Button("2");
+	Button botao3 = new Button("3");
+	Button botao4 = new Button("4");
+	Button botao5 = new Button("5");
+	Button botao6 = new Button("6");
+	Button botao7 = new Button("7");
+	Button botao8 = new Button("8");
+	Button botao9 = new Button("9");
+	
 	public Tela() {
-
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // Se não tiver esse método, mesmo que você aperte x, ele vai
-														// está exercutando // está em exercução
-		setSize(400, 600); // Tamanho da janela
+		
+		/*Criação da Janela*/
+		
+		setTitle("Calculadora"); // Titulo da calculadora
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // Se não tiver esse método, mesmo que você aperte x, ele vai está exercutando a calculadora.
+		setSize(322, 499); // Tamanho da janela
 		setLocationRelativeTo(null); // A calculadora abre no meio da tela.
+		setResizable(false); // Método para 
 		setVisible(true); // Método para ficar visivel, caso for false a janela não é mostrada.
-
-		Container container = getContentPane();
-		container.setLayout(new FlowLayout());
-
-		JButton botao1 = new JButton("1");
-		container.add(botao1, BorderLayout.CENTER);
-
-		JButton botao2 = new JButton("2");
-		container.add(botao2, BorderLayout.WEST);
-
-		JButton botao3 = new JButton("3");
-		container.add(botao3, BorderLayout.EAST);
-
-		JButton botao4 = new JButton("4");
-		container.add(botao4);
-
-		JButton botao5 = new JButton("5");
-		container.add(botao5);
-
-		JButton botao6 = new JButton("6");
-		container.add(botao6);
-
-		setVisible(true);
+		setLayout(null); // Permite setar a forma como os componentes vão se ajustar na tela ao redimensiona-la.
+		
+		
+		/*Chamada dos botões com seus métodos*/
+		
+		botao0.setBounds(2,150,74,51);
+		add(botao0);
+		
+		botao1.setBounds(77,150,74,51);
+		add(botao1);
+		
+		botao2.setBounds(152,150,74,51);
+		add(botao2);
+		
 	}
 
+	
+	
 }
